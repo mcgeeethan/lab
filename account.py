c29088d431b3f5e0c76db0b9dba8988d831773ed
mcgeeethan <emcgee@unomaha.edu>
2023-04-21 14:48:33 -0500

    added first file

---
+++ account.py
@@ -0,0 +1,16 @@
+class Account:
+    def __init__(self):
+        pass
+
+    def deposit(self, amount):
+        pass
+
+    def withdraw(self, amount):
+        pass
+
+    def get_balance(self):
+        pass
+
+    def get_name(self):
+        pass
+
