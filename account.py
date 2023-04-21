e62a5eaadd07881813c41064e271f9c53fa558b0
mcgeeethan <emcgee@unomaha.edu>
2023-04-21 16:42:18 -0500

    test files

--- account.py
+++ account.py
@@ -1,24 +1,46 @@
 class Account:
-    def __init__(self, name):
+    def __init__(self, name: str) ->None:
+        '''
+        Function to set up account object
+        :param name: Account name
+        '''
         self.__account_name = name
         self.__account_balance = 0
 
-    def deposit(self, amount):
+    def deposit(self, amount: float) -> bool:
+        '''
+        Function to deposit into the users account
+        :param amount: Currency amount
+        :return: Boolean value
+        '''
         if amount <= 0:
             return False
         else:
             self.__account_balance = self.__account_balance + amount
             return True
 
-    def withdraw(self, amount):
+    def withdraw(self, amount: float) -> bool:
+        '''
+        Fuction to withdraw currency from the users account
+        :param amount: Currecny amount
+        :return: Boolean value
+        '''
         if amount <= 0 or amount > self.__account_balance:
             return False
         else:
             self.__account_balance = self.__account_balance - amount
             return True
 
-    def get_balance(self):
+    def get_balance(self) -> None:
+        '''
+        Function to return the account balance
+        :return: account balance
+        '''
         return self.__account_balance
 
-    def get_name(self):
+    def get_name(self) -> None:
+        '''
+        Function to return the account holders name
+        :return: account holders name
+        '''
         return self.__account_name
---
+++ test_account.py
@@ -0,0 +1,39 @@
+import pytest
+from account import *
+
+class Test:
+    def setup_method(self):
+        self.a1 = Account('John')
+
+    def teardown_method(self):
+        del self.a1
+
+    def test_init(self):
+        assert self.a1.get_name() == 'John'
+
+    def test_deposit(self):
+        #negative, zero, positive
+        assert self.a1.deposit(-1.5) is False
+        assert self.a1.get_balance() == 0
+
+        assert self.a1.deposit(0) is False
+        assert self.a1.get_balance() == 0
+
+        assert self.a1.deposit(1.5) is True
+        assert self.a1.get_balance() == pytest.approx(1.5, abs=0.01)
+
+    def test_withdraw(self):
+        #negative, zero, positive invalid, positive valid
+        assert self.a1.withdraw(-1.5) is False
+        assert self.a1.get_balance() == pytest.approx(0, abs=0.01)
+
+        assert self.a1.withdraw(0) is False
+        assert self.a1.get_balance() == pytest.approx(0, abs=0.01)
+
+        assert self.a1.withdraw(100) is False
+        assert self.a1.get_balance() == pytest.approx(0, abs=0.01)
+
+        assert self.a1.deposit(1) is True
+        assert self.a1.withdraw(1) is True
+        assert self.a1.get_balance() == pytest.approx(0, abs=0.01)
+
